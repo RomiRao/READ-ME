0040eb809a3102dbf1527f31ce8ea87342e97aff
RomiRao <raoromina96@gmail.com>
2024-07-29 07:59:15 -0300

    just adding few details, still working on css home

---
+++ src/components/Home/GenreCard.jsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+export default function GenreCard() {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          Fantasy
+        </Typography>
+        <Box
+          component="img"
+          sx={{
+            height: 233,
+            width: 350,
+            maxHeight: { xs: 233, md: 167 },
+            maxWidth: { xs: 350, md: 250 },
+          }}
+          alt="The house from the offer."
+          src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+        />
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import { Carousel } from "react-responsive-carousel";
 import PromotionCard from "./PromotionCard";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Box } from "@mui/material";
+import GenreCard from "./GenreCard";
+import Grid from "@mui/material/Grid";
 
 function Home() {
   return (
-    <>
+    <Box paddingY={5} paddingX={8}>
       <Carousel
         emulateTouch
         swipeable
@@ -18,7 +21,18 @@ function Home() {
         <PromotionCard />
         <PromotionCard />
       </Carousel>
-    </>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <GenreCard />
+        </Grid>
+        <Grid item xs>
+          <GenreCard />
+        </Grid>
+        <Grid item xs>
+          <GenreCard />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
