cc29bf02ffd3b8f4b4f6dc0b2c01f8f908ce1ae1
RomiRao <raoromina96@gmail.com>
2024-07-30 12:02:35 -0300

    adjusting sizes on home layout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { Carousel } from "react-responsive-carousel";
 import PromotionCard from "./PromotionCard";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import GenreCard from "./GenreCard";
 import Grid from "@mui/material/Grid";
 
@@ -21,7 +21,7 @@ function Home() {
         <PromotionCard />
         <PromotionCard />
       </Carousel>
-      <Grid container spacing={3} marginTop={4}>
+      <Grid container spacing={3} marginY={4}>
         <Grid item xs>
           <GenreCard />
         </Grid>
@@ -32,6 +32,96 @@ function Home() {
           <GenreCard />
         </Grid>
       </Grid>
+      <Box>
+        <Typography variant="h4" padding={4} textAlign={"center"}>
+          Top Sellers
+        </Typography>
+
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2} columns={20} padding={4}>
+            <Grid xs={5} padding={3}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: "270px",
+                }}
+                alt="The house from the offer."
+                src="https://acdn.mitiendanube.com/stores/399/159/products/onepiece011-778bcfffc7d1f6acd115684066763694-640-0.jpg"
+              />
+            </Grid>
+            <Grid xs={5} padding={3}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: "270px",
+                }}
+                alt="The house from the offer."
+                src="https://acdn.mitiendanube.com/stores/399/159/products/onepiece011-778bcfffc7d1f6acd115684066763694-640-0.jpg"
+              />
+            </Grid>
+            <Grid xs={5} padding={3}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: "270px",
+                }}
+                alt="The house from the offer."
+                src="https://acdn.mitiendanube.com/stores/399/159/products/onepiece011-778bcfffc7d1f6acd115684066763694-640-0.jpg"
+              />
+            </Grid>
+            <Grid xs={5} padding={3}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: "270px",
+                }}
+                alt="The house from the offer."
+                src="https://acdn.mitiendanube.com/stores/399/159/products/onepiece011-778bcfffc7d1f6acd115684066763694-640-0.jpg"
+              />
+            </Grid>
+            <Grid xs={5} padding={3}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: "270px",
+                }}
+                alt="The house from the offer."
+                src="https://acdn.mitiendanube.com/stores/399/159/products/onepiece011-778bcfffc7d1f6acd115684066763694-640-0.jpg"
+              />
+            </Grid>
+            <Grid xs={5} padding={3}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: "270px",
+                }}
+                alt="The house from the offer."
+                src="https://acdn.mitiendanube.com/stores/399/159/products/onepiece011-778bcfffc7d1f6acd115684066763694-640-0.jpg"
+              />
+            </Grid>
+            <Grid xs={5} padding={3}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: "270px",
+                }}
+                alt="The house from the offer."
+                src="https://acdn.mitiendanube.com/stores/399/159/products/onepiece011-778bcfffc7d1f6acd115684066763694-640-0.jpg"
+              />
+            </Grid>
+            <Grid xs={5} padding={3}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: "270px",
+                }}
+                alt="The house from the offer."
+                src="https://acdn.mitiendanube.com/stores/399/159/products/onepiece011-778bcfffc7d1f6acd115684066763694-640-0.jpg"
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
     </Box>
   );
 }
